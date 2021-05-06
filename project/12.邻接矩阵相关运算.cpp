4b2ab2be4c29994dee2c63acc8de26b9a2830236
acted <1285567505@qq.com>
2021-05-06 21:35:12 +0800

    update

--- project/12.邻接矩阵相关运算.cpp
+++ project/12.邻接矩阵相关运算.cpp
@@ -2,17 +2,17 @@
 #define INF 0x3f3f3f3f
 int visited[100];
 typedef struct vertex
-{	int adjvex;//顶点编号 
-	char data[10];//顶点信息 
-}VT; 
+{	int adjvex;//顶点编号
+	char data[10];//顶点信息
+}VT;
 
 typedef struct graph{
-	int n,e;//n为顶点数，e为边数 
-	VT vexs[100];//邻接矩阵 ，顶点信息 
-	int edges[100][100];//邻接矩阵 
+	int n,e;//n为顶点数，e为边数
+	VT vexs[100];//邻接矩阵 ，顶点信息
+	int edges[100][100];//邻接矩阵
 }MG;
 
-void CG(MG &g,const int A[][100],const int n,const int e){//建立邻接矩阵 
+void CG(MG &g,const int A[][100],const int n,const int e){//建立邻接矩阵
 	int i,j;
 	g.n=n;g.e=e;
 	for(i=0;i<n;i++)
@@ -20,7 +20,7 @@ void CG(MG &g,const int A[][100],const int n,const int e){//建立邻接矩阵
 			g.edges[i][j]=A[i][j];
 }
 
-void DG(const MG g){//输出邻接矩阵 
+void DG(const MG g){//输出邻接矩阵
 	for(int i=0;i<g.n;++i){
 		for(int j=0;j<g.n;++j)
 			if(g.edges[i][j]!=INF)
@@ -30,7 +30,7 @@ void DG(const MG g){//输出邻接矩阵
 	}
 }
 
-int DG1(const MG g,int n){//无向图求节点度 
+int DG1(const MG g,int n){//无向图求节点度
 	if(n<0||n>g.n)return -1;
 	int s=0;
 	for(int i=0;i<g.n;++i)
@@ -39,7 +39,7 @@ int DG1(const MG g,int n){//无向图求节点度
 	return s;
 }
 
-int DG2(const MG g,int n){//有向图求节点度 
+int DG2(const MG g,int n){//有向图求节点度
 	if(n<0||n>g.n)return -1;
 	int s=0;
 	for(int i=0;i<g.n;++i){
@@ -49,10 +49,10 @@ int DG2(const MG g,int n){//有向图求节点度
 	return s;
 }
 
-int GG(MG &g){//销毁 
+int GG(MG &g){//销毁
 }
 
-void DFS(MG G,int v){//深度优先遍历 
+void DFS(MG G,int v){//深度优先遍历
 	int w;
 	printf("%d ",v);
 	visited[v]=1;
@@ -62,7 +62,7 @@ void DFS(MG G,int v){//深度优先遍历
 	}
 }
 
-void BFS(MG G,int v){//广度优先遍历 
+void BFS(MG G,int v){//广度优先遍历
 	int i,w,visited[100];
 	int Q[100],front=0,rear=1;
 	for(i=0;i<G.n;++i)visited[i]=0;//初始化
@@ -84,7 +84,7 @@ void BFS(MG G,int v){//广度优先遍历
 
 int main(){
 	MG g;
-	int n=5,e=7,i;//顶点数，边数 
+	int n=5,e=7,i;//顶点数，边数
 	int A[100][100]={{0 ,1  ,2  ,6  ,INF},
 					{INF,0  ,INF,4  ,5  },
 					{INF,INF,0  ,INF,3  },
@@ -96,5 +96,5 @@ int main(){
 	for(i=0;i<g.n;++i)printf("%d\t%d\n",i,DG2(g,i));
 	printf("深度优先遍历：");DFS(g,0);printf("\n");
 	printf("广度优先遍历：");BFS(g,0);printf("\n");
-	GG(g);		
+	GG(g);
 }
--- project/13.邻接表相关运算.cpp
+++ project/13.邻接表相关运算.cpp
@@ -3,69 +3,69 @@
 #define INF 0x3f3f3f3f
 int visited[100];
 typedef struct edgenide
-{	int adjvex;//边顶点编号 
-	int weight;//指向的权值 
-	struct edgenide *next;//链接其他边节点 
-}ArcNode;//边节点 
+{	int adjvex;//边顶点编号
+	int weight;//指向的权值
+	struct edgenide *next;//链接其他边节点
+}ArcNode;//边节点
 
 typedef struct vexnode{
-	char data[10];//顶点信息 
-	ArcNode *first; //指向边节点 
-}Head;//顶点 
+	char data[10];//顶点信息
+	ArcNode *first; //指向边节点
+}Head;//顶点
 
 typedef struct tu{
 	int n,e;//节点数n，边数e
-	Head adjlist[100];//顶点数组 
-}AdjGraph;//图 
+	Head adjlist[100];//顶点数组
+}AdjGraph;//图
 
-void CreateGraph(AdjGraph *&G,const int A[][100],const int n,const int e){//建立邻接表 
+void CreateGraph(AdjGraph *&G,const int A[][100],const int n,const int e){//建立邻接表
 	int i,j;ArcNode *p;
-	G=(AdjGraph *)malloc(sizeof(AdjGraph));//为图分配空间 
+	G=(AdjGraph *)malloc(sizeof(AdjGraph));//为图分配空间
 	G->n=n;G->e=e;
-	for(i=0;i<G->n;i++)G->adjlist[i].first=NULL;//初始化顶点指针 
+	for(i=0;i<G->n;i++)G->adjlist[i].first=NULL;//初始化顶点指针
 	for(i=0;i<G->n;++i)
 		for(j=0;j<G->n;++j)
 			if(A[i][j]>0&&A[i][j]<INF){
-				p=(ArcNode *)malloc(sizeof(ArcNode));//初始化边顶点 
+				p=(ArcNode *)malloc(sizeof(ArcNode));//初始化边顶点
 				p->adjvex=j;
 				p->weight=A[i][j];
-				p->next=G->adjlist[i].first;//头插法链接顶点指针和边指针 
+				p->next=G->adjlist[i].first;//头插法链接顶点指针和边指针
 				G->adjlist[i].first=p;
 			}
 }
 
-void DestroyGraph(AdjGraph *&G){//销毁邻接表 
+void DestroyGraph(AdjGraph *&G){//销毁邻接表
 	int i;
 	ArcNode *p,*q;
 	for(i=0;i<G->n;++i){
 		p=G->adjlist[i].first;
-		if(p!=NULL){//顶点指针有指向边界指针 
+		if(p!=NULL){//顶点指针有指向边界指针
 			q=p->next;
-			while(q!=NULL){//一个一个销毁 
+			while(q!=NULL){//一个一个销毁
 				free(p);
 				p=q;
 				q=q->next;
 			}
-			free(p); 
+			free(p);
 		}
 	}
-	free(G);//销毁图 
+	free(G);//销毁图
 }
 
-void DispGraph(const AdjGraph *G){//输出邻接表 
+void DispGraph(const AdjGraph *G){//输出邻接表
 	ArcNode *p;
 	int i;
 	for(i=0;i<G->n;++i){
 		printf("\t[%2d]",i);
 		p=G->adjlist[i].first;
-		if(p!=NULL){//顶点有指向边顶点 
+		if(p!=NULL){//顶点有指向边顶点
 			printf("->");
 			while(p!=NULL){
 				printf("名:%d 权:%d  ",p->adjvex,p->weight);
 				p=p->next;
 			}
 		}
-		printf("\n");//输出一个顶点链路完毕 ，换行 
+		printf("\n");//输出一个顶点链路完毕 ，换行
 	}
 }
 
@@ -76,7 +76,7 @@ int Degree1(AdjGraph *G,int v){//无向图求度
 	while(p!=NULL){
 		++s;
 		p=p->next;
-	} 
+	}
 	return s;
 }
 
@@ -84,11 +84,11 @@ int Degree2(const AdjGraph *G,const int v){//无向图求度
 	int s1=0,s2=0;
 	if(v<0||v>=G->n)return -1;
 	ArcNode *p=G->adjlist[v].first;
-	while(p!=NULL){//出度 
+	while(p!=NULL){//出度
 		++s1;
 		p=p->next;
-	} 
-	for(int i=0;i<G->n;++i){//入度 
+	}
+	for(int i=0;i<G->n;++i){//入度
 		p=G->adjlist[i].first;
 		while(p!=NULL){
 			if(p->adjvex==v)++s2;
@@ -98,20 +98,20 @@ int Degree2(const AdjGraph *G,const int v){//无向图求度
 	return s1+s2;
 }
 
-void DFS(AdjGraph *G,int v){//深度优先遍历 
+void DFS(AdjGraph *G,int v){//深度优先遍历
 	int w;
 	ArcNode *p=G->adjlist[v].first;
 	printf("%d ",v);
 	visited[v]=1;
 	while(p!=NULL){
 		w=p->adjvex;
-		if(visited[w]==0)//是否没有访问过 
+		if(visited[w]==0)//是否没有访问过
 			DFS(G,w);
 		p=p->next;
 	}
 }
 
-void BFS(AdjGraph *G,int v){//广度优先遍历 
+void BFS(AdjGraph *G,int v){//广度优先遍历
 	int i,w,visited[100];
 	int Q[100],front=0,rear=1;
 	ArcNode *p;
@@ -133,11 +133,11 @@ void BFS(AdjGraph *G,int v){//广度优先遍历
 			p=p->next;
 		}
 	}
-	 
-} 
+
+}
 
 int main(){
-	int n=5,e=7,i;//顶点数，边数 
+	int n=5,e=7,i;//顶点数，边数
 	int A[100][100]={{0 ,1  ,2  ,6  ,INF},
 					{INF,0  ,INF,4  ,5  },
 					{INF,INF,0  ,INF,3  },
